db80a65685fd4af577da7c340700a4a1f0562455
Mochammad Rezza Wikadnito <reza.wikan.dito@gmail.com>
2019-07-07 16:27:43 +0800

    fix edit product v3

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -43,7 +43,7 @@ class Product extends Model
      */
     public function getCustomPriceAttribute($value)
     {
-        if (empty($this->variations)) {
+        if (count($this->variations) > 0) {
             $price = $this->variations->map(function($variant){
                 return $variant->price;
             })->toArray();
@@ -65,7 +65,7 @@ class Product extends Model
      */
     public function getCustomWeightAttribute($value)
     {
-        if (empty($this->variations)) {
+        if (count($this->variations) > 0) {
             $weight = $this->variations->map(function($variant){
                 return $variant->weight;
             })->toArray();
